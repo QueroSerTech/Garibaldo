eb23dc6f66edf0890f72c839ab8ccb6270ce7770
Matheus Fidelis <msfidelis@outlook.com>
2017-03-03 10:23:05 -0300

    Remoção dos Daemons antigos

--- garibaldo-images.py
+++
@@ -1,6 +0,0 @@
-from google import google, images
-options = images.ImageOptions()
-options.image_type = images.ImageType.CLIPART
-options.larger_than = images.LargerThan.MP_4
-options.color = "green"
-results = google.search_images("banana", options)
